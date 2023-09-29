d2dc8526c8fbe035f28571203e04cd24bdf99c8d
Facundo Martin Gordillo <me@facundogordillo.com>
2023-09-29 22:49:41 +0200

    feat: improved how we show text

--- src/components/TransactionsList.jsx
+++ src/components/TransactionsList.jsx
@@ -5,15 +5,18 @@ function TransactionList({ transactions }) {
   const [total, setTotal] = createSignal(0);
   const [paginationProps, page] = createPagination({
     pages: Math.ceil(transactions().length / 5),
+    maxPages: 2,
   });
 
   const [data, setData] = createSignal([]);
 
   createEffect(() => {
     setTotal(
-      transactions().reduce((acc, transaction) => acc + transaction.amount, 0)
+      transactions().reduce((acc, transaction) => acc + +transaction.amount, 0).toLocaleString()
     );
-    setData(transactions().slice(page() - 1, page() * 5));
+    const initial = (page() - 1) * 5
+    const end = initial + 5
+    setData(transactions().slice(initial, end));
   });
 
   return (
@@ -25,7 +28,7 @@ function TransactionList({ transactions }) {
             class="flex justify-between items-center py-2"
             key={transaction.id}
           >
-            <span>{transaction.description}</span>
+            <span class="truncate">{transaction.description}</span>
             <span
               class={`text-${transaction.amount >= 0 ? "green" : "red"}-500`}
             >
@@ -39,7 +42,7 @@ function TransactionList({ transactions }) {
           {(props) => (
             <button
               {...props}
-              class="px-4 py-2 bg-blue-500 text-white font-semibold rounded-md"
+              class="px-3 py-2 bg-blue-500 text-white font-semibold rounded-md disabled:bg-gray-500"
             />
           )}
         </For>
--- src/utils/api.js
+++ src/utils/api.js
@@ -7,6 +7,7 @@ export function useTransactions() {
   const [data, setData] = createSignal(transactions);
 
   createEffect(() => {
+    setData(dbData.transactions);
     // const fetchedTransactions = dbData.transactions;
     // transactions = fetchedTransactions;
     // setData(fetchedTransactions);
@@ -14,7 +15,6 @@ export function useTransactions() {
 
   function addTransaction(newTransaction) {
     setData([...data(), newTransaction]);
-    dbData.transactions.push(newTransaction);
   }
 
   return [data, addTransaction];
